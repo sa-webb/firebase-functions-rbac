cbe3e1a063f65e4baf46453b46b8b89e2dc91483
sa-webb <sawebb30@gmail.com>
2020-01-16 08:23:18 -0600

    removed key config

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,12 +5,7 @@ import * as cors from 'cors';
 import * as bodyParser from 'body-parser';
 import { routesConfig } from './users/routes-config';
 
-var serviceAccount = require("../../serviceAccountKey.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://typescript-toptal.firebaseio.com"
-});
+admin.initializeApp();
 
 const app = express();
 app.use(bodyParser.json());
