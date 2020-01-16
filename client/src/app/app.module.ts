56f9189e947e85e96c2f84cc951ea6f1dad77330
sa-webb <sawebb30@gmail.com>
2020-01-16 09:31:30 -0600

    final

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,39 @@
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
+import { UsersModule } from './users/users.module';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AuthTokenHttpInterceptorProvider } from './http-interceptors/auth-token.interceptor';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([]),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    HttpClientModule,
+    UsersModule,
+    NgbModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  entryComponents: [
+    SignInComponent
+  ],
+  providers: [
+    AuthTokenHttpInterceptorProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
