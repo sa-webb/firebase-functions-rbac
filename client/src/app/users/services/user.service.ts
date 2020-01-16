3d9d932d813d7726a9606a36b505f2c4b5df7e31
sa-webb <sawebb30@gmail.com>
2020-01-16 10:01:23 -0600

    final

---
+++ client/src/app/users/services/user.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../models/user';
+import { map } from 'rxjs/operators'
+import { Observable } from 'rxjs';
+
+export type CreateUserRequest = { displayName: string, password: string, email: string, role: string }
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private baseUrl = 'https://us-central1-typescript-toptal.cloudfunctions.net/api/users'
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  get users$(): Observable<User[]> {
+    return this.http.get<{ users: User[] }>(`${this.baseUrl}`).pipe(
+      map(result => {
+        return result.users
+      })
+    )
+  }
+
+  user$(id: string): Observable<User> {
+    return this.http.get<{ user: User }>(`${this.baseUrl}/${id}`).pipe(
+      map(result => {
+        return result.user
+      })
+    )
+  }
+
+  create(user: CreateUserRequest) {
+    return this.http.post(`${this.baseUrl}`, user).pipe(
+      map(_ => { })
+    )
+  }
+}
