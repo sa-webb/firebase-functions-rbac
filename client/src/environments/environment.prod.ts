cc6566cd9e4b7e41e269e52325a641c1aca988e8
sa-webb <sawebb30@gmail.com>
2020-01-22 00:08:41 -0600

    insert keys and api endpoint

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyA0UDgRdMom_8JckyORvgrLtidBC1MYeGY",
-    authDomain: "typescript-toptal.firebaseapp.com",
-    databaseURL: "https://typescript-toptal.firebaseio.com",
-    projectId: "typescript-toptal",
-    storageBucket: "typescript-toptal.appspot.com",
-    messagingSenderId: "436391633395",
-    appId: "1:436391633395:web:bbc3b163e163a9ef01b286"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   },
-  userApi: 'https://us-central1-typescript-toptal.cloudfunctions.net/api'
+  userApi: 'https://.....cloudfunctions.net/api'
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyA0UDgRdMom_8JckyORvgrLtidBC1MYeGY",
-    authDomain: "typescript-toptal.firebaseapp.com",
-    databaseURL: "https://typescript-toptal.firebaseio.com",
-    projectId: "typescript-toptal",
-    storageBucket: "typescript-toptal.appspot.com",
-    messagingSenderId: "436391633395",
-    appId: "1:436391633395:web:bbc3b163e163a9ef01b286"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   },
   userApi: 'http://localhost:5000/typescript-toptal/us-central1/api'
 };
